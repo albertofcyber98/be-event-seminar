345267687fa388feb6fa410a13272cc316563c02
albertofcyber98 <yohanesalbert2@gmail.com>
2023-09-17 11:49:45 +0700

    reverse

--- apps/api/v1/auth/router.js
+++ apps/api/v1/auth/router.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 const router = express();
 const { signinCms } = require('./controller');
 
-router.post('/auth/signin', cors(),signinCms);
+router.post('/auth/signin', signinCms);
 
 module.exports = router;
