3c44e3e306b86eb1e0f3e7846698f790f1f32be8
albertofcyber98 <yohanesalbert2@gmail.com>
2023-09-17 11:33:00 +0700

    add cors

--- app.js
+++ app.js
@@ -27,12 +27,7 @@ const notFoundMiddleware = require('./apps/middlewares/not-found');
 const handleErrorMiddleware = require('./apps/middlewares/handler-error');
 
 
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors({ origin: true }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
