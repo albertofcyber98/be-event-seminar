e06a1bf51e35b528062f86380e047ff0ed83ae75
albertofcyber98 <yohanesalbert2@gmail.com>
2023-09-17 11:26:17 +0700

    add cors issue

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ const handleErrorMiddleware = require('./apps/middlewares/handler-error');
 
 
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
